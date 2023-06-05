8c2e42c128db14278b2d730aa1b212041bda57d2
osamaline1229 <osama98138139@gmail.com>
2023-06-05 09:51:30 +0400

    basic1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 1, b = 1;
+        bool c;
+        c = a == b;
+        Console.WriteLine(c);   
+    }
+}
