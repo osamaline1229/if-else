59ae63e7115a1492374e77e39a9645b94b5e4d31
osamaline1229 <osama98138139@gmail.com>
2023-06-05 14:25:44 +0400

    for-stars

--- Program.cs
+++ Program.cs
@@ -28,10 +28,7 @@
         for(int i= 0; i<10; i++)
         {
             
-            Console.WriteLine($"####{i}####");
-            Console.WriteLine($"#####{i}###");
-            Console.WriteLine($"######{i}##");
-
+            Console.WriteLine("*");
 
 
         }
